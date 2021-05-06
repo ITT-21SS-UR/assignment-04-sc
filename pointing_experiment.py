22686cdc04e37da51feb9e5a17653e8f51f78a35
Sarah Kurek <sarah.kurek@stud.uni-regensburg.de>
2021-05-06 21:51:32 +0200

    add circle widget

--- pointing_experiment.py
+++ pointing_experiment.py
@@ -1,60 +1,90 @@
 import sys
 from PyQt5 import QtGui, QtWidgets, QtCore
+from PyQt5.QtCore import pyqtSignal
+from math import dist
 
 
 class CircleWidget(QtWidgets.QWidget):
 
-    def __init__(self, x_pos, y_pos, size, color):
-        super(CircleWidget, self).__init__()
-        self.x_pos = x_pos
-        self.y_pos = y_pos
-        self.size = size
+    clicked = pyqtSignal()
+
+    def __init__(self, parent = None):
+        super(CircleWidget, self).__init__(parent)
+        
+        self.setAttribute(QtCore.Qt.WA_StaticContents)
+        
+        self.radius = 0
+        self.color = QtGui.QColor("Black")
+        self.setMinimumSize(50, 50)
+        self.setDiameter(50)
+
+    def setColor(self, color):
         self.color = color
+        self.update()
 
-    def draw_circle(self, q_painter):
-        q_painter.setPen(QtGui.QColor(0, 0, 0))
-        q_painter.setBrush(self.color)
-        q_painter.drawEllipse(self.x_pos, self.y_pos, self.size, self.size)
+    def setDiameter(self, diameter):
+        self.radius = int(diameter / 2)
+        self.setFixedSize(diameter, diameter)
 
-class MainWindow(QtWidgets.QTabWidget):
+    def paintEvent(self, event):
+        painter = QtGui.QPainter()
+        painter.begin(self)
+
+        painter.setPen(QtGui.QColor("Black"))
+        painter.setBrush(self.color)
+
+        rect = event.rect()
+        painter.drawEllipse(rect.x(), rect.y(), rect.width() - 1, rect.height() - 1)
+
+        painter.end()
+
+    def mousePressEvent(self, event):
+        if dist([self.radius, self.radius], [event.x(), event.y()]) <= self.radius:
+            self.clicked.emit()
+
+
+class MainWindow(QtWidgets.QWidget):
     def __init__(self):
         super(MainWindow, self).__init__()
-        self.__setup_ui()
 
-    def __setup_ui(self):
-        self.text = "Please click on the target"
         self.setGeometry(550, 200, 800, 600)
         self.setWindowTitle('FittsLawTest')
         self.setFocusPolicy(QtCore.Qt.StrongFocus)
-        # QtGui.QCursor.setPos(self.mapToGlobal(
-            # QtCore.QPoint(self.start_pos[0], self.start_pos[1])))
         self.setMouseTracking(True)
-        self.show()
 
-    def draw_background(self, event, qp, color):
-        qp.setBrush(color)
-        qp.drawRect(event.rect())
+        self.circles = []
+        self.circles.append(CircleWidget(self))
 
-    def draw_circles(self, q_painter):
-        circle = CircleWidget(50, 50, 50, QtGui.QColor(31, 60, 242))
-        circle.draw_circle(q_painter)
+        self.__setup_ui()
 
-        circle2 = CircleWidget(300, 350, 50, QtGui.QColor(242, 31, 31))
-        circle2.draw_circle(q_painter)
-    
-    def paintEvent(self, event):
-        q_painter = QtGui.QPainter()
-        q_painter.begin(self)
+    def __setup_ui(self):
+        self.setAutoFillBackground(True)
+        palette = QtGui.QGuiApplication.palette()
+        palette.setColor(QtGui.QPalette.Window, QtGui.QColor("Orange"))
+        self.setPalette(palette)
 
-        self.draw_background(event, q_painter, QtGui.QColor(245, 194, 66)) # orange
-        self.draw_circles(q_painter)
+        self.text = "Please click on the target"
+        circle = self.circles[0]
+        circle.setDiameter(100)
+        circle.setColor(QtGui.QColor("Red"))
+        circle.move(100, 100)
+        circle.clicked.connect(lambda: print("clicked"))
 
-        q_painter.end()
+    def mouseMoveEvent(self, event):
+        #print(str(event.pos()))
+        pass
 
 if __name__ == '__main__':
     # Create the Qt Application
     app = QtWidgets.QApplication(sys.argv)
+
+    # circle = CircleWidget()
+    # circle.clicked.connect(lambda: print("circle clicked"))
+    # circle.setDiameter(500)
+    # circle.show()
+
     trial = MainWindow()
+    trial.show()
     # trial = CircleWidget(50, 50, 100, QtGui.QColor(10, 10, 10))
   
     # Run the main Qt loop
