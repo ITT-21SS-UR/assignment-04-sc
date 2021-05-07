34a7d2b28287f63e6b6f3f4ee4a2cb361162e1d2
Sarah Kurek <sarah.kurek@stud.uni-regensburg.de>
2021-05-07 13:39:01 +0200

    Draw multiple circles.

--- pointing_experiment.py
+++ pointing_experiment.py
@@ -1,4 +1,4 @@
-import sys
+import sys, random
 from PyQt5 import QtGui, QtWidgets, QtCore
 from PyQt5.QtCore import pyqtSignal
 from math import dist
@@ -47,14 +47,15 @@ class CircleWidget(QtWidgets.QWidget):
 class MainWindow(QtWidgets.QWidget):
     def __init__(self):
         super(MainWindow, self).__init__()
-
+        self.width = 800
+        self.height = 600
         self.setGeometry(550, 200, 800, 600)
         self.setWindowTitle('FittsLawTest')
         self.setFocusPolicy(QtCore.Qt.StrongFocus)
         self.setMouseTracking(True)
 
         self.circles = []
-        self.circles.append(CircleWidget(self))
+        # self.circles.append(CircleWidget(self))
 
         self.__setup_ui()
 
@@ -65,12 +66,45 @@ class MainWindow(QtWidgets.QWidget):
         self.setPalette(palette)
 
         self.text = "Please click on the target"
-        circle = self.circles[0]
-        circle.setDiameter(100)
-        circle.setColor(QtGui.QColor("Red"))
-        circle.move(100, 100)
-        circle.clicked.connect(lambda: print("clicked"))
+        self.create_circles(30, 50)
+        # circle = self.circles[0]
+        # circle.setDiameter(100)
+        # circle.setColor(QtGui.QColor("Red"))
+        # circle.move(100, 100)
+        # circle.clicked.connect(lambda: print("clicked"))
 
+    
+    def create_circles(self, count, diameter):
+        min_x_y = diameter
+        max_x = self.width - diameter
+        max_y = self.height - diameter
+        pos = []
+
+        i = 0
+        while i < count:
+            x = self.genRandNum(min_x_y, max_x)
+            y = self.genRandNum(min_x_y, max_y)
+            
+            circle = CircleWidget(self)
+            circle.setDiameter(diameter)
+            circle.move(x, y)
+
+            if self.check_collision(circle, self.circles):
+                circle.destroy()
+            
+            self.circles.append(circle)
+            i += 1
+
+    def check_collision(self, current_item, items):
+        # QtWidgets.QGraphicsItem.collidesWithItem
+        for i in range (0, len(items)):
+            if current_item.rect().intersects(items[i].rect()):
+                return True
+        return False
+
+    def genRandNum(self, min, max):
+        return random.randint(min, max)
+    
     def mouseMoveEvent(self, event):
         #print(str(event.pos()))
         pass
